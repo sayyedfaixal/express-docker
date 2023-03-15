5d12015eeac044780e0f82eebb06d26f173151fe
= <=>
2023-03-15 22:15:47 +0530

    My first express app with docker

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send(`<h1>Hello from App.js, from inside a docker container...</h1>`);
+});
+
+app.listen(3000, () => {
+  console.log("App running on port 3000...");
+});
